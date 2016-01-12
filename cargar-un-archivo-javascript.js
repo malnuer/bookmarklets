897a50ddcd191adc97a5aa23d97ade44d2d8a4c4
Manuel <malnuer@gmail.com>
2016-01-12 01:14:10 +0100

    Nueva estructura de info

--- cargar-un-archivo-javascript.js
+++ cargar-un-archivo-javascript.js
@@ -2,28 +2,26 @@
  * ----------------------------------------------------------------------------
  * Añade un archivo Javascript a la página que esté cargada, para poder usarse
  * desde la consola. En el ejemplo se usa jQuery pero puede cargarse cualquier
- * otra librería. Algunos CDN para cargar librerías:
+ * otra librería. Aquí hay algunos CDN para cargar librerías:
  * - https://cdnjs.com/libraries
  * - https://developers.google.com/speed/libraries/
  * - http://www.asp.net/ajax/cdn
  * ----------------------------------------------------------------------------
+ * url - URL del archivo que queremos cargar
+ * ----------------------------------------------------------------------------
  */
 
-(function(){
+
+(function(url){
 
 	// Creamos un elemento de tipo SCRIPT
 	var s = document.createElement("script");
+
 	// Le asignamos el source de algún CDN
-	s.src = "//code.jquery.com/jquery-1.11.2.min.js";
+	s.src = url;
+
 	// Añadimos el elemento al BODY
 	document.body.appendChild(s);
 
-})();
+})("//code.jquery.com/jquery-1.11.2.min.js");
 
-
-// Cargar un archivo Javascript:
-// ----------------------------------------------------------------------------
-
-(function(){var s=document.createElement("script");s.src="//code.jquery.com/jquery-1.11.2.min.js";document.body.appendChild(s);})();
-
-// ----------------------------------------------------------------------------
